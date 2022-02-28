755158462e72a6906f4d69339927284583979e0f
Hong Jupyo <hshh0113@gmail.com>
2022-02-28 12:38:40 +0900

    1629 referring solved.

--- 20. Divide and Conquer/4. 1629.py
+++ 20. Divide and Conquer/4. 1629.py
@@ -11,10 +11,14 @@ import sys
 a, b, c = map(int, sys.stdin.readline().split())
 
 # Divide and Calculate
-def divide(tmp, a, b):
-    if b==1:
-        print(tmp)
-        return
-    divide((tmp*a)%c, a, b-1)
+def divide(a, b):
+    if b == 1:
+        return a%c
+    else:
+        tmp = divide(a, b//2)
+        if b%2 == 0:
+            return (tmp*tmp)%c
+        else:
+            return (tmp*tmp*a)%c
 
-divide(a, a, b)
+print(divide(a, b))
